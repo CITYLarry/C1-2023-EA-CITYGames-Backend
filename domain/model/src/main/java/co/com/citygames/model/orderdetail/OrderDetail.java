fb1512b0877c39c000c934aa72eb9b27f308fb55
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-04-03 14:37:59 -0500

    Generate model OrderDetail

---
+++ domain/model/src/main/java/co/com/citygames/model/orderdetail/OrderDetail.java
@@ -0,0 +1,14 @@
+package co.com.citygames.model.orderdetail;
+import lombok.Builder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class OrderDetail {
+}
---
+++ domain/model/src/main/java/co/com/citygames/model/orderdetail/gateways/OrderDetailRepository.java
@@ -0,0 +1,4 @@
+package co.com.citygames.model.orderdetail.gateways;
+
+public interface OrderDetailRepository {
+}
