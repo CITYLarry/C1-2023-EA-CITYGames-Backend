df65d098149176ff5d9778d75f842830c0ff90d3
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-04-03 09:19:53 -0500

    Define the GET endpoints for get all games and get game by id

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/citygames/api/RouterRest.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/citygames/api/RouterRest.java
@@ -1,20 +1,42 @@
 package co.com.citygames.api;
 
+import co.com.citygames.model.game.Game;
+import co.com.citygames.usecase.getallgames.GetAllGamesUseCase;
+import co.com.citygames.usecase.getgamebyid.GetGameByIdUseCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
-import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
 public class RouterRest {
     @Bean
-    public RouterFunction<ServerResponse> routerFunction(Handler handler) {
-        return route(GET("/api/usecase/path"), handler::listenGETUseCase)
-                .andRoute(POST("/api/usecase/otherpath"), handler::listenPOSTUseCase)
-                .and(route(GET("/api/otherusercase/path"), handler::listenGETOtherUseCase));
+    public RouterFunction<ServerResponse> getAllGames(GetAllGamesUseCase getAllGamesUseCase) {
+        return route(
+                GET("/api/v1/games"),
+                request -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(BodyInserters.fromPublisher(getAllGamesUseCase.get(), Game.class))
+                        .onErrorResume(throwable -> ServerResponse.noContent().build())
+        );
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> getGameById(GetGameByIdUseCase getGameByIdUseCase) {
+        return route(
+                GET("/api/v1/games/{gameId}"),
+                request -> getGameByIdUseCase.apply(request.pathVariable("gameId"))
+                        .flatMap(game -> ServerResponse.ok()
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .bodyValue(game))
+                        .onErrorResume(throwable -> ServerResponse.badRequest()
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .bodyValue(throwable.getMessage()))
+        );
     }
 }
