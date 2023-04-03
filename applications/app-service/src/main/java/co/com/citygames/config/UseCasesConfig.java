eeedb41c470a57b0532ab765e1d0f84f31847ded
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-04-03 09:27:34 -0500

    Configure the UseCasesConfig

--- applications/app-service/src/main/java/co/com/citygames/config/UseCasesConfig.java
+++ applications/app-service/src/main/java/co/com/citygames/config/UseCasesConfig.java
@@ -1,5 +1,15 @@
 package co.com.citygames.config;
 
+import co.com.citygames.model.game.gateways.GameGateway;
+import co.com.citygames.usecase.deletegame.DeleteGameUseCase;
+import co.com.citygames.usecase.getallgames.GetAllGamesUseCase;
+import co.com.citygames.usecase.getallmicrosoftgames.GetAllMicrosoftGamesUseCase;
+import co.com.citygames.usecase.getallpcgames.GetAllPcGamesUseCase;
+import co.com.citygames.usecase.getallsonygames.GetAllSonyGamesUseCase;
+import co.com.citygames.usecase.getgamebyid.GetGameByIdUseCase;
+import co.com.citygames.usecase.savegame.SaveGameUseCase;
+import co.com.citygames.usecase.updategame.UpdateGameUseCase;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -11,4 +21,44 @@ import org.springframework.context.annotation.FilterType;
         },
         useDefaultFilters = false)
 public class UseCasesConfig {
+
+        @Bean
+        public GetAllGamesUseCase getAllGamesUseCase(GameGateway gameGateway) {
+                return new GetAllGamesUseCase(gameGateway);
+        }
+
+        @Bean
+        public GetGameByIdUseCase getGameByIdUseCase(GameGateway gameGateway) {
+                return new GetGameByIdUseCase(gameGateway);
+        }
+
+        @Bean
+        public SaveGameUseCase saveGameUseCase(GameGateway gameGateway) {
+                return new SaveGameUseCase(gameGateway);
+        }
+
+        @Bean
+        public UpdateGameUseCase updateGameUseCase(GameGateway gameGateway) {
+                return new UpdateGameUseCase(gameGateway);
+        }
+
+        @Bean
+        public DeleteGameUseCase deleteGameUseCase(GameGateway gameGateway) {
+                return new DeleteGameUseCase(gameGateway);
+        }
+
+        @Bean
+        public GetAllSonyGamesUseCase getAllSonyGamesUseCase(GameGateway gameGateway) {
+                return new GetAllSonyGamesUseCase(gameGateway);
+        }
+
+        @Bean
+        public GetAllMicrosoftGamesUseCase getAllMicrosoftGamesUseCase(GameGateway gameGateway) {
+                return new GetAllMicrosoftGamesUseCase(gameGateway);
+        }
+
+        @Bean
+        public GetAllPcGamesUseCase getAllPcGamesUseCase(GameGateway gameGateway) {
+                return new GetAllPcGamesUseCase(gameGateway);
+        }
 }
