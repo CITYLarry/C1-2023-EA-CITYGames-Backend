6d3439f460290abf50ed79cbc8ea152382680d3b
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-04-03 08:22:30 -0500

    Format fixes

--- infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/citygames/mongo/MongoDBRepository.java
+++ infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/citygames/mongo/MongoDBRepository.java
@@ -2,7 +2,6 @@ package co.com.citygames.mongo;
 
 import co.com.citygames.mongo.data.GameData;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.data.repository.query.ReactiveQueryByExampleExecutor;
 
 public interface MongoDBRepository extends ReactiveMongoRepository<GameData, String> {
 }
