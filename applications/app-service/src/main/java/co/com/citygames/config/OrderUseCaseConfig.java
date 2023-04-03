596a2f46f795ba6dd0a3ca23a70b0a4c108f44ca
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-04-03 18:18:13 -0500

    Create a single use case config for the Orders

---
+++ applications/app-service/src/main/java/co/com/citygames/config/OrderUseCaseConfig.java
@@ -0,0 +1,45 @@
+package co.com.citygames.config;
+
+import co.com.citygames.model.game.gateways.GameGateway;
+import co.com.citygames.model.orderdetail.gateways.OrderDetailGateway;
+import co.com.citygames.usecase.game.deletegame.DeleteGameUseCase;
+import co.com.citygames.usecase.game.getgamebyid.GetGameByIdUseCase;
+import co.com.citygames.usecase.game.updategame.UpdateGameUseCase;
+import co.com.citygames.usecase.orderdetail.deleteorder.DeleteOrderUseCase;
+import co.com.citygames.usecase.orderdetail.getallorders.GetAllOrdersUseCase;
+import co.com.citygames.usecase.orderdetail.getorderbyid.GetOrderByIdUseCase;
+import co.com.citygames.usecase.orderdetail.saveorder.SaveOrderUseCase;
+import co.com.citygames.usecase.orderdetail.updateorder.UpdateOrderUseCase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "co.com.citygames.usecase.orderdetail")
+public class OrderUseCaseConfig {
+
+    @Bean
+    public GetAllOrdersUseCase getAllOrdersUseCase(OrderDetailGateway orderDetailGateway) {
+        return new GetAllOrdersUseCase(orderDetailGateway);
+    }
+
+    @Bean
+    public GetOrderByIdUseCase getOrderByIdUseCase(OrderDetailGateway orderDetailGateway) {
+        return new GetOrderByIdUseCase(orderDetailGateway);
+    }
+
+    @Bean
+    public SaveOrderUseCase saveOrderUseCase(OrderDetailGateway orderDetailGateway) {
+        return new SaveOrderUseCase(orderDetailGateway);
+    }
+
+    @Bean
+    public UpdateOrderUseCase updateOrderUseCase(OrderDetailGateway orderDetailGateway) {
+        return new UpdateOrderUseCase(orderDetailGateway);
+    }
+
+    @Bean
+    public DeleteOrderUseCase deleteOrderUseCase(OrderDetailGateway orderDetailGateway) {
+        return new DeleteOrderUseCase(orderDetailGateway);
+    }
+}
