be2f55269ba5ede88459f8976ebe7ce81d92f060
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-04-02 21:29:22 -0500

    Create DeleteGameUseCase

---
+++ domain/usecase/src/main/java/co/com/citygames/usecase/deletegame/DeleteGameUseCase.java
@@ -0,0 +1,16 @@
+package co.com.citygames.usecase.deletegame;
+
+import co.com.citygames.model.game.Game;
+import co.com.citygames.model.game.gateways.GameGateway;
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+@RequiredArgsConstructor
+public class DeleteGameUseCase {
+
+    private final GameGateway gameGateway;
+
+    public Mono<String> delete(String gameId) {
+        return gameGateway.deleteGame(gameId);
+    }
+}
