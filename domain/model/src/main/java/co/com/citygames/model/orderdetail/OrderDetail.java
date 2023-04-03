8cedf069163f5c44ffd3baeea80230a4d7e1e930
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-04-03 15:21:14 -0500

    Define the parameters on the OrderDetail

--- domain/model/src/main/java/co/com/citygames/model/orderdetail/OrderDetail.java
+++ domain/model/src/main/java/co/com/citygames/model/orderdetail/OrderDetail.java
@@ -1,9 +1,13 @@
 package co.com.citygames.model.orderdetail;
+import co.com.citygames.model.game.Game;
 import lombok.Builder;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.Instant;
+import java.util.List;
 
 @Getter
 @Setter
@@ -11,4 +15,11 @@ import lombok.Setter;
 @AllArgsConstructor
 @Builder(toBuilder = true)
 public class OrderDetail {
+
+    private String orderId;
+    private String customerId;
+
+    private Instant createdAt;
+
+    private List<Game> gameList;
 }
