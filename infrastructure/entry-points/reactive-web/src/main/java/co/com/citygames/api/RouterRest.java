95a02a820d7e78d6a1bf9d16319942e3996f264c
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-04-03 10:24:57 -0500

    Create the endpoint that allows to update a game

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/citygames/api/RouterRest.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/citygames/api/RouterRest.java
@@ -4,6 +4,7 @@ import co.com.citygames.model.game.Game;
 import co.com.citygames.usecase.getallgames.GetAllGamesUseCase;
 import co.com.citygames.usecase.getgamebyid.GetGameByIdUseCase;
 import co.com.citygames.usecase.savegame.SaveGameUseCase;
+import co.com.citygames.usecase.updategame.UpdateGameUseCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
@@ -13,6 +14,7 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RequestPredicates.PUT;
 import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
@@ -46,7 +48,7 @@ public class RouterRest {
     @Bean
     public RouterFunction<ServerResponse> saveGame(SaveGameUseCase saveGameUseCase) {
         return route(
-                POST("/api/v1/games/{gameId}").and(accept(MediaType.APPLICATION_JSON)),
+                POST("/api/v1/games").and(accept(MediaType.APPLICATION_JSON)),
                 request -> request.bodyToMono(Game.class)
                         .flatMap(game -> saveGameUseCase.apply(game)
                                 .flatMap(result -> ServerResponse.status(201)
@@ -55,6 +57,35 @@ public class RouterRest {
                                 .onErrorResume(throwable -> ServerResponse.badRequest()
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .bodyValue(throwable.getMessage())))
+        );
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> updateGame(UpdateGameUseCase updateGameUseCase) {
+        return route(
+                PUT("/api/v1/games/{gameId}").and(accept(MediaType.APPLICATION_JSON)),
+                request -> request.bodyToMono(Game.class)
+                        .flatMap(game -> updateGameUseCase.apply(request.pathVariable("gameId"), game)
+                                .flatMap(result -> ServerResponse.status(201)
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .bodyValue(result))
+                                .onErrorResume(throwable -> ServerResponse.badRequest()
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .bodyValue(throwable.getMessage())))
+        );
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> deleteGame(GetGameByIdUseCase getGameByIdUseCase) {
+        return route(
+                GET("/api/v1/games/{gameId}"),
+                request -> getGameByIdUseCase.apply(request.pathVariable("gameId"))
+                        .flatMap(game -> ServerResponse.ok()
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .bodyValue(game))
+                        .onErrorResume(throwable -> ServerResponse.badRequest()
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .bodyValue(throwable.getMessage()))
         );
     }
 }
