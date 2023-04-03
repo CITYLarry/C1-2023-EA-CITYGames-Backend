df03c1605d37f37496a038f0a11af570da410738
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-04-03 07:59:46 -0500

    Generate the DrivenAdapter

---
+++ infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/citygames/mongo/MongoRepositoryAdapter.java
@@ -0,0 +1,20 @@
+package co.com.citygames.mongo;
+
+import co.com.citygames.mongo.helper.AdapterOperations;
+import org.reactivecommons.utils.ObjectMapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MongoRepositoryAdapter extends AdapterOperations<Object/* change for domain model */, Object/* change for adapter model */, String, MongoDBRepository>
+// implements ModelRepository from domain
+{
+
+    public MongoRepositoryAdapter(MongoDBRepository repository, ObjectMapper mapper) {
+        /**
+         *  Could be use mapper.mapBuilder if your domain model implement builder pattern
+         *  super(repository, mapper, d -> mapper.mapBuilder(d,ObjectModel.ObjectModelBuilder.class).build());
+         *  Or using mapper.map with the class of the object model
+         */
+        super(repository, mapper, d -> mapper.map(d, Object.class/* change for domain model */));
+    }
+}
