e3cefee0d18147ca4e75298fc91166b393837e1d
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-04-03 15:18:26 -0500

    RDefine the crud methods in the gateway

--- domain/model/src/main/java/co/com/citygames/model/orderdetail/gateways/OrderDetailGateway.java
+++ domain/model/src/main/java/co/com/citygames/model/orderdetail/gateways/OrderDetailGateway.java
@@ -1,4 +1,15 @@
 package co.com.citygames.model.orderdetail.gateways;
 
+import co.com.citygames.model.game.Game;
+import co.com.citygames.model.orderdetail.OrderDetail;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 public interface OrderDetailGateway {
+
+    Flux<Game> getAllOrders();
+    Mono<Game> getOrderById(String orderId);
+    Mono<Game> saveOrder(OrderDetail orderDetail);
+    Mono<Game> updateOrder(String orderId, OrderDetail orderDetail);
+    Mono<Void> deleteOrder(String orderId);
 }
