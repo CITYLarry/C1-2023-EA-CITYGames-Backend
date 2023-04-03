3beef140e17f823be523b9ecce52b26be976504e
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-04-03 09:16:52 -0500

    Implements collection Supplier

--- domain/usecase/src/main/java/co/com/citygames/usecase/getallsonygames/GetAllSonyGamesUseCase.java
+++ domain/usecase/src/main/java/co/com/citygames/usecase/getallsonygames/GetAllSonyGamesUseCase.java
@@ -5,8 +5,10 @@ import co.com.citygames.model.game.gateways.GameGateway;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Flux;
 
+import java.util.function.Supplier;
+
 @RequiredArgsConstructor
-public class GetAllSonyGamesUseCase {
+public class GetAllSonyGamesUseCase implements Supplier<Flux<Game>> {
 
     private final GameGateway gameGateway;
 
