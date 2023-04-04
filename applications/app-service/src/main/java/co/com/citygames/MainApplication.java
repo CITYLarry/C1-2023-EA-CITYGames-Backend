d7210e98011b3da573915f9e1dadce3984fe811d
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-04-04 18:22:23 -0500

    Document the main application

--- applications/app-service/src/main/java/co/com/citygames/MainApplication.java
+++ applications/app-service/src/main/java/co/com/citygames/MainApplication.java
@@ -1,9 +1,16 @@
 package co.com.citygames;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+        title = "CITYGames API",
+        version = "1.0.0",
+        description = "API created for the final challenge at SofkaU Training League"
+))
 public class MainApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
